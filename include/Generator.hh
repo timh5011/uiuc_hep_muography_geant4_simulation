a19c4d3c3b05d5f0b259771e1ce6419467b4e15b
Tim Healy <thealy5011@gmail.com>
2024-09-23 18:42:29 -0500

    randomize position of muon

--- include/Generator.hh
+++ include/Generator.hh
@@ -5,6 +5,7 @@
 #include "G4ParticleGun.hh"
 #include "G4SystemOfUnits.hh"
 #include "G4ParticleTable.hh"
+#include "G4RandomTools.hh"
 
 class MyPrimaryGenerator : public G4VUserPrimaryGeneratorAction {
     public:
--- src/Generator.cc
+++ src/Generator.cc
@@ -10,10 +10,16 @@ MyPrimaryGenerator::~MyPrimaryGenerator() {
 
 void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent){
     G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
-    // G4String particleName = "mu+";
     G4ParticleDefinition *particle = particleTable->FindParticle("mu+");
 
-    G4ThreeVector pos(0.,0., -0.635*cm); // Defines particle initial position
+    G4double width = 61.0 * cm;  // Example width
+    G4double height = 61.0 * cm; // Example height
+
+    // Generate random x and y coordinates within the scintillator volume
+    G4double randomX = G4UniformRand() * width - (width / 2); // Centered at 0
+    G4double randomY = G4UniformRand() * height - (height / 2); 
+
+    G4ThreeVector pos(randomX, randomY, -0.635*cm); // Defines particle initial position
     G4ThreeVector mom(0.,0.,1.);// Defines particle initial momentum direction
 
     fParticleGun->SetParticlePosition(pos);
